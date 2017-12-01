df99f161a51fd966bdeb444352dcf858387fbc7e
KevinLyy <batmanthelegendly@gmail.com>
2017-11-30 22:40:58 -0500

    new book

--- 05Library/Book.java
+++ 05Library/Book.java
@@ -1,3 +1,37 @@
 Public class Book{
+    private String author;
+    private String title;
+    private String ISBN;
+
+    //null constructor 
+    public Book(){
+	
+    }
+    //actual constructor
+    public Book(String name,String booktitle,String isbn){
+	author = name;
+	title = booktitle;
+	ISBN = isbn;
+    }
+
+    //getters and setters
+    public String getAuthor(){
+	return author;
+    }
+
+    public String getTitle(){
+	return title;
+    }
+    
+    public String getISBN(){
+	return ISBN;
+    }
+    
+
+    //tostring
+    public String toString(){
+	return title + author + ISBN;
+    }
+    
 
 }
