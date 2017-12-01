dcf43acc72b90bee3505983990bb6374d23328d5
KevinLyy <batmanthelegendly@gmail.com>
2017-12-01 11:42:39 -0500

    start of ref

--- 05Library/ReferenceBook.java
+++ 05Library/ReferenceBook.java
@@ -1,3 +1,33 @@
 Public Class ReferenceBook extends LibraryBook{
+    private String collection;
+
+    //constructor
+    public ReferenceBook(String name, String bookTitle, String isbn,String callNumber, String collectionc){
+	super (name, bookTitle, isbn, callNumberc);
+	collection = collectionc;
+    }
+    //getters and setters
+    public String getAuthor(){
+	return author;
+    }
+
+    public String getTitle(){
+	return title;
+    }
+    
+    public String getISBN(){
+	return ISBN;
+    }
+
+    public String getcallNumber(){
+	return callNumber;
+    }
+
+    public String getcollection(){
+	return collection;
+    }
+
+    //void
+    
 
 }
