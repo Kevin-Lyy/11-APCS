1a0db671e6e10c37d9663ae23aed8ae0481bdc39
KevinLyy <batmanthelegendly@gmail.com>
2017-12-06 10:12:27 -0500

    mostly finished

--- barcode.java
+++ barcode.java
@@ -68,9 +68,73 @@ public class barcode implements Comparable<barcode>{
 	}
 
 	code = code + barcode[sum % 10] + "|";
-	return code; 
+	return code;
+    }
 
+    //converts the barcode to zip
+    public static String toZip(String code){
+	String zip "";
+	int checksum = 0;
+	String[] barcode = new String[10];
+	barcode[0] = "||:::";
+	barcode[1] = ":::||";
+	barcode[2] = "::|:|";
+	barcode[3] = "::||:";
+	barcode[4] = ":|::|";
+	barcode[5] = ":|:|:";
+	barcode[6] = ":||::";
+	barcode[7] = "|:::|";
+	barcode[8] = "|::|:";
+	barcode[9] = "|:|::";
+
+	if(code.length() != 32 ||!code.subString(0,1).equals("|") || !code.subString(code.length()-1,code.length()).equals("|")){
+	    throw new IllegalArgumentExpression();
+	}
+	else {
+	    for(int c = 0; c < code.length(); c++){
+		if(!code.subString(c,c+1).equals("|"))||(!code.subString(c,c+1).equals(":")){
+			throw new IllegalArgumentExcpetion();
+		    }
+	    }
+	    int x = 1;
+	    int y = 0;
+
+	    while (x< code.length() - 5 && y < 10){
+		if (code.subString(x, x+5).equals(barcode[y])){
+		    zip += y;
+		    checksum += y;
+		    x += 5;
+		    y = 0;
+		}
+		else{
+		    y++;
+		}
+	    }
+
+	    if (zip.length() > 0){
+		checksum = (checksum - Interger.parseInt(zip.subString(zip.length()-1,zip.length()))) % 10;
+	    }
+	    if (!code.subString(26,31).equals(barcode[checksum])){
+		    throw new IllegalArgumentException();
+	    }
+
+	}
+	return zip.subString(0,5);
+    }
+
+    public String getCode(){
+
+	zipToCode();
+	int checksum = 0;
+
+    }
+
+    public String getZip(){
+	return _zip;
+    }
     
 	
-
+		
+			
+			
 }
