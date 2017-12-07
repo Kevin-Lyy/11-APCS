12809f556165cce75886e71743cde491abdb2ebd
KevinLyy <batmanthelegendly@gmail.com>
2017-12-07 13:55:56 -0500

    fixed

--- 08Sorts/selectionSort.java
+++ 08Sorts/selectionSort.java
@@ -13,18 +13,20 @@ public class selectionSort{
     }
     
     public static void selectionSort(int[] data){
-	int templeast = data[0];
 	for (int c = 0; c < data.length ; c++){
-	    for (int i = 0; i < data.length;i++){
-		if (data[i] < data[templeast]){
-		    templeast = i;
+	    int next = data[c];
+	    int index = c;
+	    for (int i = c; i < data.length;i++){
+		if (data[i] < next){
+		    index = i;
 		}
 	    }
-	    swap(data,c,templeast);
+		swap(data,c,index);
 	}
     }
+ 
     public static void main(String[] args){
-	int[] testcase ={4 , 2 , 1 , 6};
+	int[] testcase ={9, 2, 4, 7, 1};
 	selectionSort.selectionSort(testcase);
 	String testpenis = "";
 	for (int c = 0; c < testcase.length; c++){
