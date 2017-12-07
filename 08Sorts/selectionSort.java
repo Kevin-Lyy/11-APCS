7cbdab1abc52e06623174297ccb2f696414eed5b
KevinLyy <batmanthelegendly@gmail.com>
2017-12-07 08:25:03 -0500

    darn

--- 08Sorts/selectionSort.java
+++ 08Sorts/selectionSort.java
@@ -5,14 +5,6 @@ public class selectionSort{
 	return "01.Ly.Kevin";
     }
 
-    public static boolean isSorted(int[]ary){
-	for(int i = 0; i < ary.length; i++){
-	    if(ary[i] > ary[i+1]){
-		    return false;
-		}
-	}
-	return true;
-    }
 
     private static void swap(int[]ary,int a, int b){
 	int c = ary[a];
@@ -22,17 +14,25 @@ public class selectionSort{
     
     public static void selectionSort(int[] data){
 	int templeast = data[0];
-	while (!isSorted(data)){
-		for (int c = 0; c < data.length; c++){
-		    for (int i = 0; i < data.length;i++){
-			if (data[i] < templeast){
-			    templeast = i;
-			}
-		    }
-		    swap(data,c,templeast);
+	for (int c = 0; c < data.length ; c++){
+	    for (int i = 0; i < data.length;i++){
+		if (data[i] < data[templeast]){
+		    templeast = i;
 		}
+	    }
+	    swap(data,c,templeast);
 	}
     }
+    public static void main(String[] args){
+	int[] testcase ={4 , 2 , 1 , 6};
+	selectionSort.selectionSort(testcase);
+	String testpenis = "";
+	for (int c = 0; c < testcase.length; c++){
+	    testpenis += testcase[c] + " ";
+	}
+	System.out.println(testpenis);
+    }
+	    
 }
 	    
 		    
