df8b9190421060011b902b5ef3c498f9d639e52a
KevinLyy <batmanthelegendly@gmail.com>
2017-12-10 02:09:23 -0500

    sorts

---
+++ 08Sorts/Sort.java
@@ -0,0 +1,11 @@
+public class Sort{
+
+    public static String name(){
+	return "11.Ly.Kevin";
+    }
+
+    void insertionSort(int[]);
+    void selectionSort(int[]);
+    void bubbleSort(int[]);
+	
+}
