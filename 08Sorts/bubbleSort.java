ab38be2a14f62adcb5f5617bff895549de178653
KevinLyy <batmanthelegendly@gmail.com>
2017-12-10 02:20:04 -0500

    bubble

--- 08Sorts/bubbleSort.java
+++ 08Sorts/bubbleSort.java
@@ -1,3 +1,26 @@
 public class bubbleSort{
 
+    public static String name(){
+	return "01.Ly.Kevin";
+    }
+
+
+    private static void swap(int[]ary,int a, int b){
+	int c = ary[a];
+	ary[a] = ary[b];
+	ary[b] = c;
+    }
+
+    public static void bubbleSort(int[] data){
+	for (intc = 0; c < data.length; c++){
+	    int next = data[c];
+	    int index = c;
+	    for (int i = c; i <data.length; i++){
+		if (data[i] < next){
+		    swap(data,index,i);
+		}
+	    }
+	}
+    }
+
 }
